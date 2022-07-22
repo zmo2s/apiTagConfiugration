c67c479cdffbc16a008a6a6df6089b1364e0969e
zmo2s <41673755+zmo2s@users.noreply.github.com>
2022-07-22 13:55:12 +0200

    refactor code , delete books sample

--- TagConfiguration2/Controllers/TagInputController.cs
+++ TagConfiguration2/Controllers/TagInputController.cs
@@ -5,76 +5,82 @@ namespace TagConfiguration2.Controllers
     using TagConfiguration2.Models;
     using TagConfiguration2.Services;
 
-  
 
-        [ApiController]
-        [Route("api/[controller]")]
-        public class TagInputController : ControllerBase
-        {
-            private readonly TagInputService _tagInputService;
 
-            public TagInputController(TagInputService tagInputService) =>
-                _tagInputService = tagInputService;
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TagInputController : ControllerBase
+    {
+        private readonly TagInputService _tagInputService;
 
-            [HttpGet]
-            public async Task<List<TagInput>> Get() =>
-                await _tagInputService.GetAsync();
+        public TagInputController(TagInputService tagInputService) =>
+            _tagInputService = tagInputService;
+
+
+        /// <summary>
+        /// Retrieves all tagInput
+        /// </summary>
+        /// <remarks>You need to insert the value in database before</remarks>
+        /// <response code="200">get result of tag</response>
+        /// <response code="400">tagInput table row is missing</response>
+        [HttpGet]
+        public async Task<List<TagInput>> Get() =>
+            await _tagInputService.GetAsync();
 
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
         [HttpGet("{id:length(24)}")]
-            public async Task<ActionResult<TagInput>> Get(string id)
+        public async Task<ActionResult<TagInput>> Get(string id)
+        {
+            var tagInput = await _tagInputService.GetAsync(id);
+
+            if (tagInput is null)
             {
-                var tagInput = await _tagInputService.GetAsync(id);
-
-                if (tagInput is null)
-                {
-                    return NotFound("toto");
-                }
-
-                return tagInput;
+                return NotFound("id doesn't exist");
             }
 
-            [HttpPost]
-            public async Task<IActionResult> Post(TagInput newTagInput)
-            {
-                await _tagInputService.CreateAsync(newTagInput);
-
-                return CreatedAtAction(nameof(Get), new { id = newTagInput.Id }, newTagInput);
-            }
-
-            [HttpPut("{id:length(24)}")]
-            public async Task<IActionResult> Update(string id, TagInput updatedTagInput)
-            {
-                var book = await _tagInputService.GetAsync(id);
-
-                if (book is null)
-                {
-                    return NotFound();
-                }
-
-                updatedTagInput.Id = book.Id;
-
-                await _tagInputService.UpdateAsync(id, updatedTagInput);
-
-                return NoContent();
-            }
-
-            [HttpDelete("{id:length(24)}")]
-            public async Task<IActionResult> Delete(string id)
-            {
-                var book = await _tagInputService.GetAsync(id);
-
-                if (book is null)
-                {
-                    return NotFound();
-                }
-
-                await _tagInputService.RemoveAsync(id);
-
-                return NoContent();
-            }
+            return tagInput;
         }
-    
+
+        [HttpPost]
+        public async Task<IActionResult> Post(TagInput newTagInput)
+        {
+            await _tagInputService.CreateAsync(newTagInput);
+
+            return CreatedAtAction(nameof(Get), new { id = newTagInput.Id }, newTagInput);
+        }
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, TagInput updatedTagInput)
+        {
+            var tagInput = await _tagInputService.GetAsync(id);
+
+            if (tagInput is null)
+            {
+                return NotFound("id doesn't exist");
+            }
+
+            updatedTagInput.Id = tagInput.Id;
+
+            await _tagInputService.UpdateAsync(id, updatedTagInput);
+
+            return NoContent();
+        }
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var tagInput = await _tagInputService.GetAsync(id);
+
+            if (tagInput is null)
+            {
+                return NotFound("id doesn't exist");
+            }
+
+            await _tagInputService.RemoveAsync(id);
+
+            return NoContent();
+        }
+    }
+
 }
 
--- TagConfiguration2/Program.cs
+++ TagConfiguration2/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using TagConfiguration2.Models;
 using TagConfiguration2.Services;
 
@@ -8,14 +10,33 @@ builder.Services.Configure<TagsConfigurationDatabaseSettings>(
 builder.Configuration.GetSection("TagsConfigurationDatabase"));
 builder.Services.AddSingleton<TagInputService>();
 
-
-builder.Services.Configure<BookStoreDatabaseSettings>(
-    builder.Configuration.GetSection("BookStoreDatabase"));
-builder.Services.AddSingleton<BooksService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Calculate tag autonomy API",
+        Description = "An ASP.NET Core Web API for calculate tag autonomy",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Site web Ela innovation",
+            Url = new Uri("https://elainnovation.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // using System.Reflection;
+  //  var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+  //  options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
